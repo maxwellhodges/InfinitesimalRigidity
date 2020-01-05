ec5278b628b15cebfc42613cfc3b669af0b23bf0
Max Hodges <maxhodges@Maxs-MacBook-Pro.local>
2020-01-05 19:42:49 +0000

    need to implement pruning indexing code

--- infrig.py
+++ infrig.py
@@ -2,8 +2,8 @@ import pyximport; pyximport.install()
 import numpy as np
 from infrig_helpers import find_triangle
 
-# TODO code won't currently work if node is part of multiple clusters
 
+# TODO code won't currently work properly if node is part of multiple clusters
 def edge_list_to_adjacency_list(N, edge_list):
     adjacency_list = [[] for node in range(N)]
     for edge in edge_list:
@@ -30,122 +30,129 @@ def generate_rigidity_matrix(coords, edge_list, N):
     return R
 
 
-def generate_inf_motions(rigidity_matrix):
+def generate_inf_motions(rigidity_matrix, N_current):
     # there are two cases here: edges > 2*nodes (thin) or edges < 2*nodes (fat) TODO look into sparse SVD here
     u, s, vt = np.linalg.svd(rigidity_matrix)
     singular_vals, = np.where(s < 10e-15)
-    if R.shape[0] > R.shape[1]:
+    if rigidity_matrix.shape[0] > rigidity_matrix.shape[1]:
         inf_motions = vt[singular_vals, :]
         # scale the inf motions by the size of the network for numerical stability
-        inf_motions = N * inf_motions
-    else:
-        inf_motions = np.vstack((vt[singular_vals, :], vt[R.shape[0]:]))
+        inf_motions = N_current * inf_motions
+    else:  # TODO also scale this
+        inf_motions = np.vstack((vt[singular_vals, :], vt[rigidity_matrix.shape[0]:]))
 
     return inf_motions
 
 # this part of the code will be replaced by function that generates lattice
 # number of nodes
-N = 8
-toy_structure_coords = np.array([[0, 0], [1, 0], [0, 1], [1, 1], [2, 2], [2, 3], [3, 2], [3, 3]])
-edge_list = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (4, 6), (5, 6), (5, 7), (6, 7)]
-
-adjacency_list = edge_list_to_adjacency_list(N, edge_list)
-
-node_list = list(range(N))
-
-# prune any nodes that have fewer than 2 neighbours as cannot be part of rigid cluster
-floppy_nodes = list(node for node, neighs in enumerate(adjacency_list) if len(neighs) < 2)
-# current_node_list will keep track of total number of unassigned nodes.  Algorithms ends when it is empty.
-current_node_list = [node for node in node_list if node not in floppy_nodes]
-current_edge_list = [pair for pair in edge_list if all(node in current_node_list for node in pair)]
-
-# TODO think i need an initial mapping for the remaining nodes once the dangling nodes are removed
-N_current = len(current_node_list)
-current_coords = np.array([coord for i, coord in enumerate(toy_structure_coords) if i in current_node_list])
-# only want remaining nodes after pruning
-current_adjacency_list = [neigh for i, neigh in enumerate(adjacency_list) if i in current_node_list]
+# N = 8
+# coords = np.array([[0, 0], [1, 0], [0, 1], [1, 1], [2, 2], [2, 3], [3, 2], [3, 3]])
+# edge_list = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (4, 6), (5, 6), (5, 7), (6, 7)]
 
 
-# create rigidity matrix for structure, ignoring floppy nodes
-R = generate_rigidity_matrix(current_coords, current_edge_list, N_current)
-# get the infinitesimal motions
-inf_motions = generate_inf_motions(R)
+def cluster_decomp(coords, edge_list):
 
-""" The following section will go inside a while loop.  Need to consider that indices need to be matched back to 
-node ids after each iteration as nodes are removed from the node_list """
-cluster_dict = {}
-cluster_index = 0
-while current_node_list:
-    # the current_node_list is the way to map back from list indices to the node value
+    N = coords.shape[0]
 
-    # need this format as input into find_triangle function
-    current_deg_array, current_neigh_array = to_degree_array_and_neighbour_array(current_adjacency_list)
+    adjacency_list = edge_list_to_adjacency_list(N, edge_list)
 
-    # TODO may have to change this logic now
-    triangle_nodes = find_triangle(current_deg_array, current_neigh_array,
-                                   np.array(current_node_list).astype(np.intc))
-    if triangle_nodes == -1:
-        floppy_nodes.extend(current_node_list)
-        break
+    node_list = list(range(N))
 
-    # convert triangle_nodes to current indices
-    triangle_current_indices = [current_node_list.index(item) for item in triangle_nodes]
+    # prune any nodes that have fewer than 2 neighbours as cannot be part of rigid cluster
+    # TODO need to put this line back once pruning code is done
+    # TODO this actually breaks find_triangle if it's not there...
+    # floppy_nodes = list(node for node, neighs in enumerate(adjacency_list) if len(neighs) < 2)
+    floppy_nodes = []
+    # current_node_list will keep track of total number of unassigned nodes.  Algorithms ends when it is empty.
+    current_node_list = [node for node in node_list if node not in floppy_nodes]
+    current_edge_list = [pair for pair in edge_list if all(node in current_node_list for node in pair)]
 
-    # get three rigid motions from inf motions for the triangle
-    rigid_triangle_coords = current_coords[triangle_current_indices, :]
-    triangle_centre_of_mass = np.sum(rigid_triangle_coords, axis=0)/3
-    distance_from_com = current_coords - triangle_centre_of_mass
-    node_rotations = np.cross(distance_from_com, np.array([0, 0, 1]))[:, 0:2]  # note shape of node_rotations is (N, 2)
-
-    # normalise by triangle nodes vector length
-    triangle_rotations = node_rotations[np.array(triangle_current_indices)]
-    normalisation_factor = np.linalg.norm(triangle_rotations.flatten())
-
-    # rigid motions for all the nodes that are left
-    node_rotations_norm = node_rotations.flatten()/normalisation_factor  # flatten node_rotations out here
-    node_translations_x_norm = np.tile([1/np.sqrt(3), 0], N_current)
-    node_translations_y_norm = np.tile([0, 1/np.sqrt(3)], N_current)
-
-    # rigid motions for triangle
-    triangle_rotations_norm = triangle_rotations.flatten()/normalisation_factor
-    triangle_translation_x_norm = np.tile([1/np.sqrt(3), 0], 3)
-    triangle_translation_y_norm = np.tile([0, 1/np.sqrt(3)], 3)
-
-    # get indices as we have now flattened the motion vectors
-    triangle_flattened_indices = [j for i in triangle_current_indices for j in (2*i, 2*i + 1)]
-    inf_triangle_motions = inf_motions[:, triangle_flattened_indices]
-
-    # get the translation and rotation components for the nodes for each of the infinitesimal motions
-    x_components = inf_triangle_motions.dot(triangle_translation_x_norm)  # TODO can vectorise this part too
-    y_components = inf_triangle_motions.dot(triangle_translation_y_norm)
-    rot_components = inf_triangle_motions.dot(triangle_rotations_norm)
-
-    # test = inf_triangle_motions - ((x_components.reshape(5, 1) * triangle_translation_x_norm) +
-    #                                (y_components.reshape(5, 1) * triangle_translation_y_norm) +
-    #                                (rot_components.reshape(5, 1) * triangle_rotations_norm))
-
-    final_differences = inf_motions - ((x_components.reshape(inf_motions.shape[0], 1) * node_translations_x_norm) +
-                                       (y_components.reshape(inf_motions.shape[0], 1) * node_translations_y_norm) +
-                                       (rot_components.reshape(inf_motions.shape[0], 1) * node_rotations_norm))
-
-    temp = final_differences.reshape(inf_motions.shape[0], *current_coords.shape)
-    absolute_node_distances = np.linalg.norm(temp, axis=2)
-
-    rigid_nodes_boolean = np.max(absolute_node_distances < 1e-5, axis=0)
-    rigid_indices = np.where(rigid_nodes_boolean == 1)[0]
-    rigid_nodes = np.array(current_node_list)[rigid_indices]
-
-    cluster_dict[cluster_index] = rigid_nodes
-    cluster_index += 1
-
-    # remove those nodes found to be rigid
-    current_node_list = [node for node in current_node_list if node not in rigid_nodes]
     N_current = len(current_node_list)
-    current_adjacency_list = [list(set(neigh).intersection(set(current_node_list)))
-                              for i, neigh in enumerate(adjacency_list) if i in current_node_list]
+    current_coords = np.array([coord for i, coord in enumerate(coords) if i in current_node_list])
+    # only want remaining nodes after pruning
+    current_adjacency_list = [neigh for i, neigh in enumerate(adjacency_list) if i in current_node_list]
 
-    current_coords = np.array([coord for i, coord in enumerate(toy_structure_coords) if i in current_node_list])
 
-    # remove motions corresponding to rigid nodes found in current loop
-    inf_motions = inf_motions[:, np.repeat(~rigid_nodes_boolean, 2)]
+    # create rigidity matrix for structure TODO need to add logic to reindex after removing floppy nodes
+    R = generate_rigidity_matrix(current_coords, current_edge_list, N_current)
+    # get the infinitesimal motions
+    inf_motions = generate_inf_motions(R, N_current)
 
+    """ The following section will go inside a while loop.  Need to consider that indices need to be matched back to 
+    node ids after each iteration as nodes are removed from the node_list """
+    cluster_dict = {}
+    cluster_index = 0
+    while current_node_list:
+        # the current_node_list is the way to map back from list indices to the node value
+
+        # need this format as input into find_triangle function
+        current_deg_array, current_neigh_array = to_degree_array_and_neighbour_array(current_adjacency_list)
+
+        triangle_nodes = find_triangle(current_deg_array, current_neigh_array,
+                                       np.array(current_node_list).astype(np.intc))
+        if triangle_nodes == -1:
+            floppy_nodes.extend(current_node_list)
+            break
+
+        # convert triangle_nodes to current indices
+        triangle_current_indices = [current_node_list.index(item) for item in triangle_nodes]
+
+        # get three rigid motions from inf motions for the triangle
+        rigid_triangle_coords = current_coords[triangle_current_indices, :]
+        triangle_centre_of_mass = np.sum(rigid_triangle_coords, axis=0)/3
+        distance_from_com = current_coords - triangle_centre_of_mass
+        node_rotations = np.cross(distance_from_com, np.array([0, 0, 1]))[:, 0:2]  # note shape of node_rotations is (N, 2)
+
+        # normalise by triangle nodes vector length
+        triangle_rotations = node_rotations[np.array(triangle_current_indices)]
+        normalisation_factor = np.linalg.norm(triangle_rotations.flatten())
+
+        # rigid motions for all the nodes that are left
+        node_rotations_norm = node_rotations.flatten()/normalisation_factor  # flatten node_rotations out here
+        node_translations_x_norm = np.tile([1/np.sqrt(3), 0], N_current)
+        node_translations_y_norm = np.tile([0, 1/np.sqrt(3)], N_current)
+
+        # rigid motions for triangle
+        triangle_rotations_norm = triangle_rotations.flatten()/normalisation_factor
+        triangle_translation_x_norm = np.tile([1/np.sqrt(3), 0], 3)
+        triangle_translation_y_norm = np.tile([0, 1/np.sqrt(3)], 3)
+
+        # get indices as we have now flattened the motion vectors
+        triangle_flattened_indices = [j for i in triangle_current_indices for j in (2*i, 2*i + 1)]
+        inf_triangle_motions = inf_motions[:, triangle_flattened_indices]
+
+        # get the translation and rotation components for the nodes for each of the infinitesimal motions
+        x_components = inf_triangle_motions.dot(triangle_translation_x_norm)  # TODO can vectorise this part too
+        y_components = inf_triangle_motions.dot(triangle_translation_y_norm)
+        rot_components = inf_triangle_motions.dot(triangle_rotations_norm)
+
+        # test = inf_triangle_motions - ((x_components.reshape(5, 1) * triangle_translation_x_norm) +
+        #                                (y_components.reshape(5, 1) * triangle_translation_y_norm) +
+        #                                (rot_components.reshape(5, 1) * triangle_rotations_norm))
+
+        final_differences = inf_motions - ((x_components.reshape(inf_motions.shape[0], 1) * node_translations_x_norm) +
+                                           (y_components.reshape(inf_motions.shape[0], 1) * node_translations_y_norm) +
+                                           (rot_components.reshape(inf_motions.shape[0], 1) * node_rotations_norm))
+
+        temp = final_differences.reshape(inf_motions.shape[0], *current_coords.shape)
+        absolute_node_distances = np.linalg.norm(temp, axis=2)
+
+        rigid_nodes_boolean = np.max(absolute_node_distances < 1e-5, axis=0)
+        rigid_indices = np.where(rigid_nodes_boolean == 1)[0]
+        rigid_nodes = np.array(current_node_list)[rigid_indices]
+
+        cluster_dict[cluster_index] = rigid_nodes
+        cluster_index += 1
+
+        # remove those nodes found to be rigid
+        current_node_list = [node for node in current_node_list if node not in rigid_nodes]
+        N_current = len(current_node_list)
+        current_adjacency_list = [list(set(neigh).intersection(set(current_node_list)))
+                                  for i, neigh in enumerate(adjacency_list) if i in current_node_list]
+
+        current_coords = np.array([coord for i, coord in enumerate(coords) if i in current_node_list])
+
+        # remove motions corresponding to rigid nodes found in current loop
+        inf_motions = inf_motions[:, np.repeat(~rigid_nodes_boolean, 2)]
+
+    return cluster_dict  # TODO also return floppy nodes
--- scratch.py
+++ scratch.py
@@ -1,36 +1,106 @@
-import pyximport; pyximport.install()
-import cy_testing
+# import pyximport; pyximport.install()
+# import cy_testing
+# import numpy as np
+#
+#
+# def edge_list_to_adjacency_list(N, edge_list):
+#     adjacency_list = [[] for node in range(N)]
+#     for edge in edge_list:
+#         adjacency_list[edge[0]].append(edge[1])
+#         adjacency_list[edge[1]].append(edge[0])
+#
+#     return adjacency_list
+#
+#
+# def to_degree_array_and_neighbour_array(adjacency_list):
+#     degree_array = np.array([len(neighs) for neighs in adjacency_list])
+#     neighbour_array = np.array([j for i in adjacency_list for j in i])
+#
+#     return degree_array.astype(np.intc), neighbour_array.astype(np.intc)
+#
+#
+# N = 8
+#
+# edge_list = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (4, 6), (5, 6), (5, 7), (6, 7)]
+# adjacency_list = edge_list_to_adjacency_list(N, edge_list)
+#
+# current_nodes = np.array(range(N)).astype(np.intc)[4:]
+# adj_list = adjacency_list[4:]
+# current_adj_list = [list(set(neigh).intersection(set(current_nodes)))
+#                               for i, neigh in enumerate(adjacency_list) if i in current_nodes]
+#
+# degs, neighs = to_degree_array_and_neighbour_array(current_adj_list)
+#
+# triangles = []
+# for i in range(50):
+#     triangles.append(cy_testing.find_triangle(degs, neighs, current_nodes))
+
+
+import itertools
 import numpy as np
+import matplotlib.pyplot as plt
+
+points = np.array(list(itertools.product(range(6), range(6))))
+num_points = points.shape[0]
+
+x = points[:, 0]
+y = points[:, 1]
+
+# get total edge list
+import scipy.spatial.distance as ssd
+distances = ssd.cdist(points, points)
+indices = np.where(distances < np.sqrt(2) + 0.0001)
+edges_repeats = np.vstack([*indices]).T
+
+def unique(sequence):
+    seen = set()
+    return [x for x in sequence if not (x in seen or seen.add(x))]
 
 
-def edge_list_to_adjacency_list(N, edge_list):
-    adjacency_list = [[] for node in range(N)]
-    for edge in edge_list:
-        adjacency_list[edge[0]].append(edge[1])
-        adjacency_list[edge[1]].append(edge[0])
+allowed_edges = unique(tuple(pair) for pair in edges_repeats.tolist())
 
-    return adjacency_list
+# plot figure with edges present
+def plot_lattice(x, y, allowed_edges=None):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    if allowed_edges is not None:
+        for edge in allowed_edges:
+            ax.plot(x[list(edge)], y[list(edge)], 'b-')
+    ax.plot(x, y, 'ro')
+    plt.show()
+
+# take random subsets of the allowed edges
+def subset_edges(edges, fraction_edges=0.5):
+    num_allowed_edges = len(edges)
+    np.random.seed(42)
+    num_subset_edges = int(fraction_edges * num_allowed_edges)
+    subset_indices = np.random.permutation(num_allowed_edges)[:num_subset_edges]
+    subset_edges = [allowed_edges[i] for i in subset_indices]
+    return subset_edges
 
 
-def to_degree_array_and_neighbour_array(adjacency_list):
-    degree_array = np.array([len(neighs) for neighs in adjacency_list])
-    neighbour_array = np.array([j for i in adjacency_list for j in i])
+# plot figure with subset edges present
+subset_of_edges = subset_edges(allowed_edges, 0.2)
+plot_lattice(x, y, subset_of_edges)
 
-    return degree_array.astype(np.intc), neighbour_array.astype(np.intc)
+import infrig
+out = infrig.cluster_decomp(points, subset_of_edges)
 
+colors=["#008000", "#FF0000", "#0000FF"]
 
-N = 8
+col = [None] * num_points
+for key in out.keys():
+    for node in out[key]:
+        col[node] = colors[key]
 
-edge_list = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5), (4, 6), (5, 6), (5, 7), (6, 7)]
-adjacency_list = edge_list_to_adjacency_list(N, edge_list)
+col = ["#808080" if v is None else v for v in col]
 
-current_nodes = np.array(range(N)).astype(np.intc)[4:]
-adj_list = adjacency_list[4:]
-current_adj_list = [list(set(neigh).intersection(set(current_nodes)))
-                              for i, neigh in enumerate(adjacency_list) if i in current_nodes]
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+for edge in subset_of_edges:
+    ax.plot(x[list(edge)], y[list(edge)], c='grey', alpha=0.4)
+for i in range(num_points):
+    ax.scatter(x[i], y[i], color=col[i], s=100)
+plt.show()
+# plt.savefig("edge_prob_03.png")
 
-degs, neighs = to_degree_array_and_neighbour_array(current_adj_list)
-
-triangles = []
-for i in range(50):
-    triangles.append(cy_testing.find_triangle(degs, neighs, current_nodes))
